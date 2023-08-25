5d99b534530d6344b92dd81d48bd0c10e7eb1c00
dnaniel213 <dnaniel213@gmail.com>
2023-08-25 16:01:46 +0800

    - build service fix

--- Assets/Capriccioso/Scripts/Editor/BuildMenu.cs
+++ Assets/Capriccioso/Scripts/Editor/BuildMenu.cs
@@ -7,7 +7,7 @@ namespace Capriccioso
 {
     public class BuildScript
     {
-
+        
         [MenuItem("Build/Build All")]
         public static void BuildAll()
         {
@@ -20,10 +20,9 @@ namespace Capriccioso
         [MenuItem("Build/Build Server (Windows)")]
         public static void BuildWindowsServer()
         {
-            string buildPath = "Builds";
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = new[] { "Assets/_Scenes/Offline.unity" , "Assets/_Scenes/Lobby.unity"};
-            buildPlayerOptions.locationPathName = buildPath + "/Windows/Server/HaphAngel-Server.exe";
+            buildPlayerOptions.scenes = BuildService.Instance.Scenes;
+            buildPlayerOptions.locationPathName = $"{BuildService.Instance.BuildPath}/Windows/Server/{BuildService.Instance.Name}-{BuildService.Instance.Version}-Windows-Server.exe";
             buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
             buildPlayerOptions.options = BuildOptions.CompressWithLz4HC | BuildOptions.EnableHeadlessMode;
 
@@ -35,10 +34,9 @@ namespace Capriccioso
         [MenuItem("Build/Build Server (Linux)")]
         public static void BuildLinuxServer()
         {
-            string buildPath = "Builds";
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = new[] { "Assets/_Scenes/Offline.unity" , "Assets/_Scenes/Lobby.unity"};
-            buildPlayerOptions.locationPathName = buildPath + "/Linux/Server/haphazard_linux_server.x86_64";
+            buildPlayerOptions.scenes = BuildService.Instance.Scenes;
+            buildPlayerOptions.locationPathName = $"{BuildService.Instance.BuildPath}/Linux/Server/{BuildService.Instance.Name}-{BuildService.Instance.Version}-Linux-Server.x86_64";
             buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
             buildPlayerOptions.options = BuildOptions.CompressWithLz4HC | BuildOptions.EnableHeadlessMode;
 
@@ -50,10 +48,9 @@ namespace Capriccioso
         [MenuItem("Build/Build Client (Windows)")]
         public static void BuildWindowsClient()
         {
-            string buildPath = "Builds";
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = new[] { "Assets/_Scenes/Offline.unity", "Assets/_Scenes/Lobby.unity", "Assets/_Scenes/main.unity" };
-            buildPlayerOptions.locationPathName = buildPath + "/Windows/Client/haphazard_windows_client.exe";
+            buildPlayerOptions.scenes = BuildService.Instance.Scenes;
+            buildPlayerOptions.locationPathName = $"{BuildService.Instance.BuildPath}/Windows/Client/{BuildService.Instance.Name}-{BuildService.Instance.Version}-Windows-Client.exe";
             buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
             buildPlayerOptions.options = BuildOptions.CompressWithLz4HC;
 
@@ -65,10 +62,9 @@ namespace Capriccioso
         [MenuItem("Build/Build Client (HTML5)")]
         public static void BuildWebClient()
         {
-            string buildPath = "Builds";
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = new[] { "Assets/_Scenes/Offline.unity", "Assets/_Scenes/Lobby.unity", "Assets/_Scenes/main.unity" };
-            buildPlayerOptions.locationPathName = buildPath + "/WebGL/Client/HaphAngel-WebGL-Client";
+            buildPlayerOptions.scenes = BuildService.Instance.Scenes;
+            buildPlayerOptions.locationPathName = $"{BuildService.Instance.BuildPath}/WebGL/Client/{BuildService.Instance.Name}-{BuildService.Instance.Version}-WebGL-Client";
             buildPlayerOptions.target = BuildTarget.WebGL;
             buildPlayerOptions.options = BuildOptions.CompressWithLz4HC;
 
